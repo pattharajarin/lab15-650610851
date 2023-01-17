cdb6b68476df5049a9a6f40347f1eaf2e327709d
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-01-17 16:40:28 +0700

    ok

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for(int i=1; i<N; i++){
+		for(int j=i; j>0; j--){
+			if(d[j]>d[j-1]){
+				double swap=d[j];
+				d[j]=d[j-1];
+				d[j-1]=swap;
+			}
+		}
+	cout << "Pass " << i << ":" ;
+	for(int x=0; x<N ; x++){
+		cout << d[x] << " ";
+	}
+	cout << "\n";
+	}
 
 }
 
