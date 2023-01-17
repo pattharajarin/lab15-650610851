31cedcfcf1ce933d50f4c0da4aecb67adf68367c
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-01-17 17:26:02 +0700

    ok

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,48 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+	for(int i=0; i<N; i++){
+		cout <<"Row "<< i+1 << ": " ;
+
+		for(int j=0; j<N; j++){
+			cin >> a[i][j] ;
+		}
+	}
+}
+
+void showMatrix(const bool a[][N]){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (i == 0 or j == 0 or i == N - 1 or j == N - 1)
+			{
+				b[i][j] = 0;
+			}
+			else
+			{
+
+				if (a[i][j] >= a[i - 1][j] && a[i][j] >= a[i + 1][j] && a[i][j] >= a[i][j - 1] && a[i][j] >= a[i][j + 1])
+				{
+					b[i][j] = 1;
+				}
+				else
+				{
+					b[i][j] = 0;
+				}
+			}
+		}
+	}
+}
